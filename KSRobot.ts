f14ec5df7be246e2c07590241e1bdb53002468fc
kaise-ksrobot <arthur@kaise.com.tw>
2019-05-19 20:32:34 +0800

    v0.4.1

---
+++ KSRobot.ts
@@ -0,0 +1,25 @@
+// Auto-generated. Do not edit.
+
+
+    //% color=#00A6F0 weight=19 icon="\uf1eb" block="KSRobot_IR"
+namespace KSRobot_IR {
+
+    //% blockId=ir_received_left_event
+    //% block="on |%btn| button pressed"
+    //% shim=KSRobotIR::onPressEvent
+    export function onPressEvent(btn: RemoteButton, body: Action):void{
+        return
+    }
+
+    //% blockId=ir_init
+    //% block="connect ir receiver to %pin" 
+    //% shim=KSRobotIR::init
+    export function init(pin: Pins): void{
+      return
+    }
+
+ 
+
+}
+
+// Auto-generated. Do not edit. Really.
---
+++ README.md
@@ -0,0 +1,14 @@
+ # KSRobot_IR for micro-bit
+
+Extension for KSRobot_IR
+
+Note: IR receiver for KSRobot receiver
+
+## License
+
+MIT
+
+## Supported targets
+
+* for PXT/microbit
+(The metadata above is needed for package search.)
